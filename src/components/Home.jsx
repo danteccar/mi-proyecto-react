228b7faaeda8c771c95f2f5e6e722020a11bacb3
danteccarhuas <ccarhuas.dante@gmail.com>
2025-02-26 18:12:53 -0500

    instalado gh pages

---
+++ src/components/Home.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import Contents from "./Contents";
+
+const Home = () => {
+  const productos = [
+    { id: 1, nombre: "Smartphone" },
+    { id: 2, nombre: "Laptop" },
+    { id: 3, nombre: "Tablet" },
+    { id: 4, nombre: "Auriculares" },
+    { id: 5, nombre: "SmartTV" },
+    { id: 6, nombre: "Altavoz" },
+  ];
+
+  return (
+    <>
+      <h1>Home</h1>
+      <Contents />
+
+      <h2>Productos</h2>
+      <ul className="list-group">
+        {productos.map((producto) => (
+          <Link
+            to={"/producto/" + producto.nombre}
+            className="list-group-item list-group-item-action"
+          >
+            {producto.nombre}
+          </Link>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/components/Producto.jsx
@@ -0,0 +1,16 @@
+import { useParams } from "react-router-dom";
+import Contents from "./Contents";
+
+function Producto() {
+
+    const params = useParams();
+
+  return (
+    <>
+      <h1>{params.nombreParam}</h1>
+      <Contents />
+    </>
+  );
+}
+
+export default Producto;
